8042c2da25c75d91d8634c45732d9cd06f9f3c2b
Igor Minoru Tamura <igor.minoru@outlook.com>
2024-01-17 14:07:56 -0300

    Criação das entidades Cidade e Estado

--- src/main/java/com/osprasoft/cursomc/CursomcApplication.java
+++ src/main/java/com/osprasoft/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.osprasoft.cursomc.domain.Categoria;
+import com.osprasoft.cursomc.domain.Cidade;
+import com.osprasoft.cursomc.domain.Estado;
 import com.osprasoft.cursomc.domain.Produto;
 import com.osprasoft.cursomc.repositories.CategoriaRepository;
+import com.osprasoft.cursomc.repositories.CidadeRepository;
+import com.osprasoft.cursomc.repositories.EstadoRepository;
 import com.osprasoft.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -17,9 +21,12 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -43,6 +50,20 @@ public class CursomcApplication implements CommandLineRunner {
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Caetano do Sul", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
 	}
 
 }
