bee97c242d6ddfef6c4b58b17676925516cba4ea
Igor Minoru Tamura <igor.minoru@outlook.com>
2024-01-17 13:21:18 -0300

    Implementação de exceção para busca de categoria inexistente

--- src/main/java/com/osprasoft/cursomc/services/CategoriaService.java
+++ src/main/java/com/osprasoft/cursomc/services/CategoriaService.java
@@ -1,13 +1,10 @@
 package com.osprasoft.cursomc.services;
 
 import java.util.Optional;
-
-import javax.swing.text.html.Option;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.osprasoft.cursomc.repositories.CategoriaRepository;
+import com.osprasoft.cursomc.services.exception.ObjectNotFoundException;
 import com.osprasoft.cursomc.domain.Categoria;
 
 @Service
@@ -18,7 +15,9 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id) {
         Optional < Categoria > obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+            "Objeto não encontrado! Id: " + id + ", Tipo: " 
+            + Categoria.class.getName()));
     }
     
 }
