ac784b04e85009dc87b971cb91aa7f42324a9977
Igor Minoru Tamura <igor.minoru@outlook.com>
2024-01-31 12:28:10 -0300

    Listar Categorias utilizando DTO

--- src/main/java/com/osprasoft/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/osprasoft/cursomc/resources/CategoriaResource.java
@@ -2,6 +2,7 @@ package com.osprasoft.cursomc.resources;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.osprasoft.cursomc.domain.Categoria;
+import com.osprasoft.cursomc.dto.CategoriaDTO;
 import com.osprasoft.cursomc.services.CategoriaService;
 
 @RestController
@@ -51,9 +53,11 @@ public class CategoriaResource {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity < List < Categoria >> findAll() {
+    public ResponseEntity < List < CategoriaDTO >> findAll() {
         List < Categoria > list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List < CategoriaDTO > listDTO = list.stream().map(
+            obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
     
 }
