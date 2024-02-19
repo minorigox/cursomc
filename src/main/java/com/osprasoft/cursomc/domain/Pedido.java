4c6df87278d8d981c7323fcbe9a0b1bfdee2c7a8
Igor Minoru Tamura <igor.minoru@outlook.com>
2024-02-19 17:23:43 -0300

    Correção de máscara de data e hora

--- src/main/java/com/osprasoft/cursomc/domain/Pedido.java
+++ src/main/java/com/osprasoft/cursomc/domain/Pedido.java
@@ -2,6 +2,7 @@ package com.osprasoft.cursomc.domain;
 
 import java.io.Serializable;
 import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Locale;
@@ -53,11 +54,12 @@ public class Pedido implements Serializable {
     @Override
     public String toString() {
         NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         StringBuilder builder = new StringBuilder();
         builder.append("Pedido nº: ");
         builder.append(getId());
-        builder.append(", Instante= ");
-        builder.append(getInstante());
+        builder.append(", Instante: ");
+        builder.append(sdf.format(getInstante()));
         builder.append(", Cliente: ");
         builder.append(getCliente().getNome());
         builder.append(", Situação do pagamento: ");
