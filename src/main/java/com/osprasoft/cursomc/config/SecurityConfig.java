c9891448290d6f5712e878cac4cd713bc8dca941
Igor Minoru Tamura <igor.minoru@outlook.com>
2024-02-21 17:07:29 -0300

    Configuração do spring security usando token jwt

---
+++ src/main/java/com/osprasoft/cursomc/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.osprasoft.cursomc.config;
+
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private Environment env;
+
+    private static final String[] PUBLIC_MATCHERS = {
+        "/h2-console/**"
+    };
+
+    private static final String[] PUBLIC_MATCHERS_GET = {
+        "/produtos/**",
+        "/categorias/**"
+    };
+
+     @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {    
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers((headers) -> headers.disable());
+        }
+
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(
+            auth -> auth.requestMatchers(
+                HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+                .requestMatchers(PUBLIC_MATCHERS).permitAll()
+                .anyRequest().authenticated()).sessionManagement(
+                    sm -> sm.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+}
--- src/main/java/com/osprasoft/cursomc/services/PedidoService.java
+++ src/main/java/com/osprasoft/cursomc/services/PedidoService.java
@@ -5,14 +5,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
-
-import com.osprasoft.cursomc.repositories.ClienteRepository;
 import com.osprasoft.cursomc.repositories.ItemPedidoRepository;
 import com.osprasoft.cursomc.repositories.PagamentoRepository;
 import com.osprasoft.cursomc.repositories.PedidoRepository;
-import com.osprasoft.cursomc.repositories.ProdutoRepository;
 import com.osprasoft.cursomc.services.exception.ObjectNotFoundException;
-import com.osprasoft.cursomc.domain.Cliente;
 import com.osprasoft.cursomc.domain.ItemPedido;
 import com.osprasoft.cursomc.domain.PagamentoBoleto;
 import com.osprasoft.cursomc.domain.Pedido;
