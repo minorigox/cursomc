e5ab0af8733b5436c7b8ad1afbbddd1cefa4c8c7
Igor Minoru Tamura <igor.minoru@outlook.com>
2024-03-07 12:04:54 -0300

    Configurando cors para PUT e DELETE

--- src/main/java/com/osprasoft/cursomc/security/SecurityConfig.java
+++ src/main/java/com/osprasoft/cursomc/security/SecurityConfig.java
@@ -16,6 +16,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -63,15 +66,14 @@ public class SecurityConfig {
         return http.build();
     }
 
-    // Prof. Nelio Alves
-    // @Bean
-    // CorsConfigurationSource corsConfigurationSource() {
-    // final UrlBasedCorsConfigurationSource source = new
-    // UrlBasedCorsConfigurationSource();
-    // source.registerCorsConfiguration("/**", new
-    // CorsConfiguration().applyPermitDefaultValues());
-    // return source;
-    // }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
