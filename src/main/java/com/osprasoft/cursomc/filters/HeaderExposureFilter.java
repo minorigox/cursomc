698f6c948b26b33191fa814119a7e6685d93141b
Igor Minoru Tamura <igor.minoru@outlook.com>
2024-03-07 12:22:09 -0300

    Expondo o location na resposta

---
+++ src/main/java/com/osprasoft/cursomc/filters/HeaderExposureFilter.java
@@ -0,0 +1,24 @@
+package com.osprasoft.cursomc.filters;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class HeaderExposureFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.addHeader("access-control-expose-headers", "location");
+        chain.doFilter(request, response);
+    }    
+}
